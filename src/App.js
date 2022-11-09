6a4e1c11daf1dda7e8d83e37c0a2abc8f6688b9e
MaciekDuda <maciejduda1626@gmail.com>
2022-11-09 17:44:37 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+	Timeline,
+	Logos,
+	Features,
+	Navbar,
+} from "../../re-tweet-app/src/components";
+import {
+	Footer,
+	Header,
+	Tweets,
+	About,
+	Next,
+} from "../../re-tweet-app/src/containers";
+import "./App.css";
+
+const App = () => {
+	return (
+		<div className="App">
+			<div className="gradient__bg">
+				<div className="bg__tweet-layer">
+					<Navbar />
+					<Header />
+					<Logos />
+					<Tweets />
+					<About />
+					<Next />
+					<Footer />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
